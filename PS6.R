99a527f4cc663f5537de9ac582405f6f145cc040
szayed <54649152+szayed174@users.noreply.github.com>
2021-03-13 21:29:11 -0500

    Most recent 3.13

--- PS6.R
+++ PS6.R
@@ -5,6 +5,7 @@
 #QMB 6912 Capstone Project in Business Analytics
 #Problem Set #6
 #
+#Salma Zayed
 ##################################################
 # 
 #Flyreels data set
@@ -13,6 +14,7 @@
 # 
 #working directory
 #print(getwd())
+#wd<-"C:/Users/szaye/OneDrive/Documents/UCF/Spring 2021/QMB6912/ProblemSet6"
 # Set data directory.
 data_dir <- 'Data'
 
@@ -41,7 +43,7 @@ library(gclus)
 
 #Selecting relevant variables:
 #install.packages("relaimpo")
-library("relaimpo")
+library(relaimpo)
 
 # regression models.
 #install.packages("texreg")
@@ -59,12 +61,14 @@ library(dplyr)
 #compare models
 #install.packages("MASS")
 library(MASS)
+#install.packages("kdensity", dependencies = TRUE)
+#library(kdensity)
 
 #install.packages("plyr")
 library(plyr)
 
 #install.packages("gcookbook")
-#library(gcookbook)
+library(gcookbook)
 
 #Box-Cox transformation package
 #install.packages("faraway")
@@ -74,7 +78,31 @@ library(faraway)
 data <- read.csv("FlyReels.csv")
 print(data)
 
-#Assuming that a reel is like a cylinder where Volume = π R^2L  
+#Changing Yes and No characters into numeric 0s and 1
+data$Sealed <- revalue(data$Sealed, c("Yes"=1))
+data$Sealed <- revalue(data$Sealed, c("No"=0))
+head(data$Sealed)
+data$Sealed<-as.numeric(as.character(data$Sealed))
+str(data$Sealed)
+
+data$Machined <- revalue(data$Machined, c("Yes"=1))
+data$Machined <- revalue(data$Machined, c("No"=0))
+head(data$Machined)
+data$Machined<-as.numeric(as.character(data$Machined))
+str(data$Machined)
+
+#Changing Country to numeric. Where USA=1 China & Korea=2
+Country_num <- revalue(data$Country, c("USA"=1,"China"=2,"Korea"=2))
+#Adding Country_num  as a column to the dataframe
+attach(data)
+data$Country_num =Country_num
+detach(data)
+head(data,50)
+
+data$Country_num<-as.numeric(as.character(data$Country_num))
+str(data)
+
+#Assuming that a reel is like a cylinder where Volume = ?? R^2L  
 density <- ((data$Weight)/(pi * (data$Diameter/2)^2 * data$Width))
 
 #Adding density as a column to the dataframe
@@ -92,172 +120,155 @@ head(data)
 print(summary(data))
 
 
-###############     Muliple Variable Regression Mixed    ##################################################3
+#filter relevant columns
+data_num <- data %>%
+  select(3,4,5,6,7,9,10,11)
+head(data_num)
 
-modelMultiple <- lm(Price ~ Weight+Diameter+Width+Sealed+Country+Machined+density, data)
+#Print Pairs:
+pairs(data_num)
+# function gpairs in "gpairs"
+#install.packages("gpairs")
+library("gpairs")
+gpairs(data_num)
+
+###############     Multiple Variable Regression Mixed    ##################################################3
+
+modelMultiple <- lm(Price ~ Brand+Weight+Diameter+Width+Sealed+Country+Machined+density, data)
 print(modelMultiple)
-coef(modelMultiple)
 summary(modelMultiple)
-
-modelMultiple.Country <- lm(Price ~ Country, data)
-summary(modelMultiple.Country)
-
-modelMultiple.Country2 <- glm(Price ~ Country, data, family="gaussian")
-summary(modelMultiple.Country2)
-
-
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
 plot(modelMultiple)
 
 
-#Changing Yes and No characters into numeric 0s and 1
-data$Sealed <- revalue(data$Sealed, c("Yes"=1))
-data$Sealed <- revalue(data$Sealed, c("No"=0))
-head(data$Sealed)
-data$Sealed<-as.numeric(as.character(data$Sealed))
-str(data$Sealed)
-
-data$Machined <- revalue(data$Machined, c("Yes"=1))
-data$Machined <- revalue(data$Machined, c("No"=0))
-head(data$Machined)
-data$Machined<-as.numeric(as.character(data$Machined))
-str(data$Machined)
-
-
-#Changing Country to numeric. Where USA=1 China=2 Korea=3
-data$Country <- revalue(data$Country, c("USA"=1))
-data$Country <- revalue(data$Country, c("China"=2))
-data$Country <- revalue(data$Country, c("Korea"=3))
-head(data$Country)
-data$Country<-as.numeric(as.character(data$Country))
-str(data)
-
-###############     Muliple Variable Regression Numerics    ##################################################3
-modelMultiple1 <- lm(Price ~ Weight+Diameter+Width+Sealed+Country+Machined+density, data)
-print(modelMultiple1)
-coef(modelMultiple1)
-summary(modelMultiple1)
+modelMultiple.log <- lm(log(Price) ~ Brand+Weight+Diameter+Width+Sealed+Country+Machined+density, data)
+print(modelMultiple.log)
+summary(modelMultiple.log)
 
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(modelMultiple1)
+plot(modelMultiple.log)
 
-
-#filter relevant columns
-data_filtered <- data %>% 
-  select_(3,4,5,6,7,8,9,10)
-head(data_filtered)
-
-#let's make the data have mean of zero and standard deviation of 1 standardize the data (normalize)
-dataScaled <- as.data.frame(scale(data_filtered))
-print(dataScaled[1:7,])
-
-pairs(dataScaled)
-
-#Running the Muliple Variable Regression with scaled data
-#modelMultiple1_scaled <- lm(Price ~ Weight+Diameter+Width+Sealed+Country+Machined+density, dataScaled)
-#print(modelMultiple1_scaled)
-#coef(modelMultiple1_scaled)
-#summary(modelMultiple1_scaled)
-
-#layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-#plot(modelMultiple1_scaled)
-
-
-
-##Choosing the most significant variables (Diameter, Sealed, Country, Machined)
-#Multiple Variable Regression
-modelMultiple2 <- lm(Price ~ Diameter+Sealed+Country+Machined, data)
-print(modelMultiple2)
-summary(modelMultiple2)
+modelMultiple3 <- lm((((Price^0.3131)-1)/0.3131) ~ Brand+Weight+Diameter+Width+Sealed+Country+Machined+density, data)
+summary(modelMultiple3)
 
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(modelMultiple2)
+plot(modelMultiple3)
 
 
-#Multiple Variable Regression scaled
-#modelMultiple2_scaled <- lm(Price ~ Diameter+Sealed+Country+Machined, dataScaled)
-#print(modelMultiple2_scaled)
-#summary(modelMultiple2_scaled)
+modelMultiple4 <- lm(Price ~.-Price-Country_num, data)
+summary(modelMultiple4)
 
-#layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-#plot(modelMultiple2_scaled)
-
-data$Country = as.factor(data$Country)
-str(data)
+##################    AIC    #######################
 
 #model selection with AIC, simple to use
-#modelMultiple1
-step1 <- stepAIC(modelMultiple1, direction="both")
+#modelMultiple
+step1 <- stepAIC(modelMultiple, direction="both")
 step1$anova # display results
 
-#modelMultiple1_scaled
-#step2 <- stepAIC(modelMultiple1_scaled, direction="both")
-#step2$anova # display results
+AIC1 <- AIC(modelMultiple)
+print(AIC1)
+#modelMultiple.log
+step2 <- stepAIC(modelMultiple.log, direction="both")
+step2$anova # display results
 
-#modelMultiple2 - has the highest AIC
-step3 <- stepAIC(modelMultiple2, direction="both")
+AIC2 <- AIC(modelMultiple.log)
+print(AIC2)
+
+#modelMultiple3
+step3 <- stepAIC(modelMultiple3, direction="both")
 step3$anova # display results
 
-#modelMultiple2_scaled
-#step4 <- stepAIC(modelMultiple2_scaled, direction="both")
-#step4$anova # display results
+AIC3 <- AIC(modelMultiple3)
+print(AIC3)
+
+#############       Likelihood        ########
+
+calcLogLikelihood <- function(model_param, dependent_var_param, lambda_param) {
+  eps = 1/50
+  y <- dependent_var_param
+  xqr <- model_param$qr
+  n <- length(y)
+  y <- y/exp(mean(log(y))) #y/geometric_mean(y)
+  #logy <- log(y)
+  
+  xl <- loglik <- as.vector(lambda)
+  m <- length(xl)
+  if (abs(lambda_param) > eps) {
+    yt <- (y^lambda_param - 1)/lambda_param
+  }  else {
+    yt <- log(y)
+  }
+  
+  logLikelihood <- -n/2 * log(sum(qr.resid(xqr, yt)^2))
+  
+  
+  return(logLikelihood)
+}
 
 
-#Looking more into variable importance:
-#modelMultiple2
-boot <- boot.relimp(modelMultiple2, b = 1000, rank = TRUE, diff = TRUE, rela = TRUE)
-booteval.relimp(boot)
-plot(booteval.relimp(boot,sort=TRUE))
+test <- calcLogLikelihood(modelMultiple, data$Price, 2)
+test <- calcLogLikelihood(modelMultiple, data$Price, 0)
 
-#modelMultiple1
-boot2 <- boot.relimp(modelMultiple1, b = 1000, rank = TRUE, diff = TRUE, rela = TRUE)
-booteval.relimp(boot2)
-plot(booteval.relimp(boot2,sort=TRUE))
+test <- calcLogLikelihood(modelMultiple.log, data$Price, 2)
+test <- calcLogLikelihood(modelMultiple.log, data$Price, 0)
 
+test <- calcLogLikelihood(modelMultiple3, data$Price, 2)
+test <- calcLogLikelihood(modelMultiple3, data$Price, 0)
+
+
+logLik(modelMultiple)
+logLik(modelMultiple.log)
+logLik(modelMultiple3)
+
+#For modelMultiple1
+lambda = seq(-2, 2, 1/20)
+i=1;
+logLikelihood_y <- as.vector(lambda)
+for (x_lambda in lambda){
+  logLikelihood_y[i] <- calcLogLikelihood(modelMultiple, data$Price, x_lambda)
+  i = i + 1
+}
+plot(lambda, logLikelihood_y)
+
+#For modelMultiple LOG
+lambda2 = seq(-5, 5, 1/20)
+i=1;
+logLikelihood_y2 <- as.vector(lambda2)
+for (x_lambda in lambda2){
+  logLikelihood_y2[i] <- calcLogLikelihood(modelMultiple.log, log(data$Price), x_lambda)
+  i = i + 1
+}
+plot(lambda2, logLikelihood_y2)
 
 ###############    Box-Cox Transformation     ##################################################
 
-#model 1 plot
-layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(modelMultiple1)
-
 #box-cox for model 1
-#bc_m1=boxcox(modelMultiple1)
-
-bc_m1=boxcox(modelMultiple1,lambda=seq(-3,3))
+bc_m1=boxcox(modelMultiple,lambda=seq(-2,2, by=0.1))
 print(bc_m1)
-
-#extract best lambda - close to the above lambda -3,3 if we round up
-best.lam_m1=bc_m1$x[which(bc_m1$y==max(bc_m1$y))]
-print(best.lam_m1)
-
-#boxcox says to transform the data using inverse
-modelMultiple1_inv=lm((Price)^-1~Weight+Diameter+Width+Sealed+Country+Machined+density, data)
-layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(modelMultiple1_inv)
-
-#comparing to plot(modelMultiple1), with the inverse model we have a straighter trend lines, thus it has improved normality, constant variance and linearity.  
-
-#box-cox for model 2
-#model 2 plot
-layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(modelMultiple2)
+  #extract best lambda - close to the above lambda -3,3 if we round up
+  best.lam_m1=bc_m1$x[which(bc_m1$y==max(bc_m1$y))]
+  print(best.lam_m1)
 
 #box-cox for model2
-
-bc_m2=boxcox(modelMultiple2,lambda=seq(-3,3))
+bc_m2=boxcox(modelMultiple.log,lambda=seq(-5,5,by = 0.1))
 print(bc_m2)
+  #extract best lambda - close to the above lambda -3,3 if we round up
+  best.lam_m2=bc_m2$x[which(bc_m2$y==max(bc_m2$y))]
+  print(best.lam_m2)
 
-#extract best lambda - close to the above lambda -3,3 if we round up
-best.lam_m2=bc_m2$x[which(bc_m2$y==max(bc_m2$y))]
-print(best.lam_m2)
+#box-cox for model3
+bc_m3=boxcox(modelMultiple3,lambda=seq(-3,3,by = 0.1))
+print(bc_m3)
+  #extract best lambda - close to the above lambda -3,3 if we round up
+  best.lam_m3=bc_m3$x[which(bc_m3$y==max(bc_m3$y))]
+  print(best.lam_m3)  
+  
+#box-cox for model4
+bc_m4=boxcox(modelMultiple4,lambda=seq(-3,3,by = 0.1))
+print(bc_m4)
+  #extract best lambda - close to the above lambda -3,3 if we round up
+  best.lam_m4=bc_m4$x[which(bc_m4$y==max(bc_m4$y))]
+  print(best.lam_m4)
 
-#maybe model 2 would be best with a lambda of 2.. rounding down best lambda
-bc_m2=boxcox(modelMultiple2,lambda=seq(-2,2))
+  
 
-
-#boxcox says to transform the data using inverse
-modelMultiple2_inv=lm((Price)^-1~Diameter+Sealed+Country+Machined, data)
-
-layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(modelMultiple2_inv)
